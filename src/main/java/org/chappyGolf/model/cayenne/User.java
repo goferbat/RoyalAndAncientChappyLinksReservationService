dd6bbac597cdb9d88e803b507cba5d98cda00ba5
goferbat <dhalbert@bu.edu>
2025-09-25 12:12:29 -0500

    moving to cayenne along AND aws

---
+++ src/main/java/org/chappyGolf/model/cayenne/User.java
@@ -0,0 +1,9 @@
+package org.chappyGolf.model.cayenne;
+
+import org.chappyGolf.model.cayenne.auto._User;
+
+public class User extends _User {
+
+    private static final long serialVersionUID = 1L;
+
+}
