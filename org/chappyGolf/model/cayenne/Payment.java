c419cb60d9fcfe73a54a3eb31354dfce902f660b
goferbat <dhalbert@bu.edu>
2025-09-25 15:33:41 -0500

    moving to cayenne along AND aws

---
+++ org/chappyGolf/model/cayenne/Payment.java
@@ -0,0 +1,9 @@
+package org.chappyGolf.model.cayenne;
+
+import org.chappyGolf.model.cayenne.auto._Payment;
+
+public class Payment extends _Payment {
+
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ org/chappyGolf/model/cayenne/TeeTime.java
@@ -0,0 +1,9 @@
+package org.chappyGolf.model.cayenne;
+
+import org.chappyGolf.model.cayenne.auto._TeeTime;
+
+public class TeeTime extends _TeeTime {
+
+    private static final long serialVersionUID = 1L;
+
+}
